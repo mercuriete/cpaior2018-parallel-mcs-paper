a12b0d29201a698fd7b70edcd41fd0b9121b774c
Ciaran McCreesh <ciaran.mccreesh@glasgow.ac.uk>
2017-08-08 12:17:29 +0100

    Slightly betterer

--- paper/asify.sh
+++ paper/asify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-join -1 2 -2 2 <(grep '^[^#]' $1 | sort -k2 ) <(grep '[^#]' $2 | sort -k2 ) | cut -d' ' -f1,2,4 | sort -n -k3,3
+join -1 2 -2 2 <(grep 'i$' $1 | sort -k2 ) <(grep 'i$' $2 | sort -k2 ) | cut -d' ' -f1,2,4 | sort -n -k3,3
